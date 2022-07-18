d1cd3f608e1e50ef05b62cc92eac14717367ea1d
Jesal Patel <jesalpatelx@gmail.com>
2022-07-18 12:09:53 -0400

    Added image cache

---
+++ Flip/Components/CoverView.swift
@@ -0,0 +1,39 @@
+//
+//  CoverView.swift
+//  Flip
+//
+//
+
+import CachedAsyncImage
+import SwiftUI
+
+struct CoverView: View {
+    let url: URL
+
+    var body: some View {
+        CachedAsyncImage(url: url, urlCache: .imageCache) { phase in
+            thumbnailImage(phase)
+        }
+    }
+
+    func thumbnailImage(_ phase: AsyncImagePhase) -> some View {
+        Group {
+            switch phase {
+            case .empty, .failure:
+                Image(systemName: "book.closed")
+                    .resizable()
+                    .font(.body.weight(.ultraLight))
+                    .scaledToFit()
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+            @unknown default:
+                Image(systemName: "book.closed")
+                    .resizable()
+                    .font(.body.weight(.ultraLight))
+                    .scaledToFit()
+            }
+        }
+    }
+}
