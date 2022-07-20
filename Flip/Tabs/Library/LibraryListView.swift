db75f42728f3c5e0f37f5ace58783422c751a64f
Jesal Patel <jesalpatelx@gmail.com>
2022-07-20 11:46:51 -0400

    Factor out LibraryListView

---
+++ Flip/Tabs/Library/LibraryListView.swift
@@ -0,0 +1,72 @@
+//
+//  LibraryListView.swift
+//  Flip
+//
+//  Created by Jesal Patel on 7/19/22.
+//
+
+import SwiftUI
+
+struct LibraryListView: View {
+
+    let sortOrder: Book.SortOrder
+    let bookFilter: Book.BookFilter
+
+    @EnvironmentObject var dataController: DataController
+    @Environment(\.managedObjectContext) var managedObjectContext
+
+    let booksRequest: FetchRequest<Book>
+    var books: [Book] {
+        Array(booksRequest.wrappedValue)
+
+    }
+    var searchedBooks: [Book] {
+        if searchText.isEmpty {
+            return books
+        } else {
+            return books.filter { $0.bookTitle.contains(searchText) || $0.bookAuthor.contains(searchText) }
+        }
+    }
+
+    @State private var searchText = ""
+
+    init(sortOrder: Book.SortOrder, bookFilter: Book.BookFilter) {
+        self.sortOrder = sortOrder
+        self.bookFilter = bookFilter
+
+        booksRequest = FetchRequest<Book>(
+            entity: Book.entity(),
+            sortDescriptors: Book.getSort(sortOrder),
+            predicate: Book.getPredicate(bookFilter)
+        )
+    }
+
+    var body: some View {
+        List {
+            ForEach(searchedBooks) { book in
+                NavigationLink(value: book) {
+                    LibraryRowView(book: book)
+                }
+            }
+            .onDelete { offsets in
+                delete(offsets)
+            }
+        }
+        .searchable(text: $searchText, prompt: "Search")
+    }
+
+    func delete(_ offsets: IndexSet) {
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+        for offset in offsets {
+            let book = searchedBooks[offset]
+            dataController.delete(book)
+        }
+        dataController.save()
+    }
+}
+
+struct LibraryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryListView(sortOrder: .author, bookFilter: .allBooks)
+    }
+}
--- Flip/Tabs/Library/LibraryView.swift
+++ Flip/Tabs/Library/LibraryView.swift
@@ -11,31 +11,14 @@ struct LibraryView: View {
     static let tag: String = "Library"
 
     @EnvironmentObject var dataController: DataController
-    @Environment(\.managedObjectContext) var managedObjectContext
+//    @Environment(\.managedObjectContext) var managedObjectContext
 
-    @FetchRequest(
-        sortDescriptors: [SortDescriptor(\Book.author, order: .forward)]
-    ) private var books: FetchedResults<Book>
+    @SceneStorage("sortOrder") private var sortOrder: Book.SortOrder = .author
+    @SceneStorage("bookFilter") private var bookFilter: Book.BookFilter = .allBooks
 
-    @State private var showingSortOrder = false
-//    @SceneStorage("sortOrder") private var sortOrder: Book.SortOrder = .author
-//    @SceneStorage("bookFilter") private var bookFilter: Book.BookFilter = .allBooks
-    @State private var sortOrder: Book.SortOrder = .author
-    @State private var bookFilter: Book.BookFilter = .allBooks
-    @State private var selectedBook: Book?
-    @State private var searchText = ""
     @State private var path = [Book]()
-
-    var query: Binding<String> {
-        Binding {
-            searchText
-        } set: { newValue in
-            searchText = newValue
-            books.nsPredicate = newValue.isEmpty
-            ? nil
-            : NSPredicate(format: "title CONTAINS[c] %@ OR author CONTAINS[c] %@", newValue, newValue)
-        }
-    }
+    @State private var selectedBook: Book?
+    @State private var showingSortOrder = false
 
     var filterToolbarItem: some View {
         Button {
@@ -61,50 +44,29 @@ struct LibraryView: View {
 
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(books) { book in
-                    NavigationLink(value: book) {
-                        LibraryRowView(book: book)
+            LibraryListView(sortOrder: sortOrder, bookFilter: bookFilter)
+                .navigationTitle(navigationTitleText())
+                .navigationDestination(for: Book.self) { book in
+                    LibraryBookView(book: book)
+                }
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        filterToolbarItem
+                        sortToolbarItem
                     }
                 }
-                .onDelete { offsets in
-                    delete(offsets)
+                .onContinueUserActivity(CSSearchableItemActionType, perform: loadSpotlightBook)
+                .confirmationDialog("Sort Books", isPresented: $showingSortOrder) {
+                    Button("Author") { sortOrder = .author }
+                    Button("Title") { sortOrder = .title }
+                    Button("Page Count") { sortOrder = .pageCount }
+                    Button("Finish Date") { sortOrder = .readDate }
+                    Button("Publication Date") { sortOrder = .publicationDate }
+                    Button("Cancel", role: .cancel) { }
+                } message: {
+                    Text("Sort Books")
                 }
             }
-            .navigationTitle(navigationTitleText())
-            .navigationDestination(for: Book.self) { book in
-                LibraryBookView(book: book)
-            }
-            .searchable(text: query, prompt: "Search")
-            .toolbar {
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    filterToolbarItem
-                    sortToolbarItem
-                }
-            }
-            .onContinueUserActivity(CSSearchableItemActionType, perform: loadSpotlightBook)
-            .confirmationDialog("Sort Books", isPresented: $showingSortOrder) {
-                Button("Author") { sortOrder = .author }
-                Button("Title") { sortOrder = .title }
-                Button("Page Count") { sortOrder = .pageCount }
-                Button("Finish Date") { sortOrder = .readDate }
-                Button("Publication Date") { sortOrder = .publicationDate }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Sort Books")
-            }
-            .onChange(of: sortOrder) { _ in updateSort() }
-            .onChange(of: bookFilter) { _ in updateFilter() }
-        }
-    }
-
-    func delete(_ offsets: IndexSet) {
-        hapticFeedback(style: .medium)
-        for offset in offsets {
-            let book = books[offset]
-            dataController.delete(book)
-        }
-        dataController.save()
     }
 
     func selectBook(with identifier: String) {
@@ -118,36 +80,6 @@ struct LibraryView: View {
         if let uniqueIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String {
             selectBook(with: uniqueIdentifier)
         }
-    }
-
-    func updateFilter() {
-        var predicate: NSPredicate
-        switch bookFilter {
-        case .allBooks:
-            predicate = NSPredicate(value: true)
-        case .readBooks:
-            predicate = NSPredicate(format: "read = true")
-        case .unreadBooks:
-            predicate = NSPredicate(format: "read = false")
-        }
-        books.nsPredicate = predicate
-    }
-
-    func updateSort() {
-        var descriptor: [SortDescriptor<Book>]
-        switch sortOrder {
-        case .title:
-            descriptor = [SortDescriptor(\Book.title, order: .forward)]
-        case .author:
-            descriptor = [SortDescriptor(\Book.author, order: .forward)]
-        case .pageCount:
-            descriptor = [SortDescriptor(\Book.pageCount, order: .forward)]
-        case .readDate:
-            descriptor = [SortDescriptor(\Book.dateRead, order: .forward)]
-        case .publicationDate:
-            descriptor = [SortDescriptor(\Book.publicationDate, order: .forward)]
-        }
-        books.sortDescriptors = descriptor
     }
 
     func hapticFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
@@ -171,7 +103,7 @@ struct LibraryView_Previews: PreviewProvider {
     static var dataController = DataController.preview
     static var previews: some View {
         LibraryView()
-            .environment(\.managedObjectContext, dataController.container.viewContext)
+//            .environment(\.managedObjectContext, dataController.container.viewContext)
             .environmentObject(dataController)
     }
 }
