d92829729c1a474fc9065eab9aa2b390f97bc9b9
Jesal Patel <jesalpatelx@gmail.com>
2022-07-26 15:26:01 -0400

    Added additional custom icons

--- Flip/Tabs/Settings/IconPickerView.swift
+++ Flip/Tabs/Settings/IconPickerView.swift
@@ -10,16 +10,13 @@ import SwiftUI
 struct IconPickerView: View {
     var body: some View {
         Form {
-            IconButtonView(
-                iconName: nil,
-                iconImage: "AppIconImage",
-                iconText: "Default"
-            )
-            IconButtonView(
-                iconName: "AppIconGreen",
-                iconImage: "AppIconGreenImage",
-                iconText: "Green"
-            )
+            IconButtonView(iconName: nil, iconImage: "AppIconPurpleImage", iconText: "Purple")
+            IconButtonView(iconName: "AppIconGreen", iconImage: "AppIconGreenImage", iconText: "Green")
+            IconButtonView(iconName: "AppIconBlue", iconImage: "AppIconBlueImage", iconText: "Blue")
+            IconButtonView(iconName: "AppIconCandy", iconImage: "AppIconCandyImage", iconText: "Cotton Candy")
+            IconButtonView(iconName: "AppIconPink", iconImage: "AppIconPinkImage", iconText: "Pink")
+            IconButtonView(iconName: "AppIconTeal", iconImage: "AppIconTealImage", iconText: "Teal")
+            IconButtonView(iconName: "AppIconYellow", iconImage: "AppIconYellowImage", iconText: "Yellow")
         }
         .navigationTitle("Icon Options")
     }
