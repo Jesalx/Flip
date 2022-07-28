b6aa4d2d0a1a36559bf800355e3e8ccbd96dba0d
Jesal Patel <jesalpatelx@gmail.com>
2022-07-27 21:54:31 -0400

    Upgrade to Nuke 11

--- Flip/Components/CoverView.swift
+++ Flip/Components/CoverView.swift
@@ -8,20 +8,11 @@
 import SwiftUI
 import NukeUI
 
-// Downgraded from Nuke 11 -> Nuke 10 + NukeUI because Nuke 11 has a bug where
-// ImageDecoding throws an error when decoding certain images while this doesn't
-// happen on Nuke 10. Nuke 11 is only a couple days old at the time of using so
-// periodically check if Nuke 11 will work as expected.
-
 struct CoverView: View {
-    let url: String
-
-    init(url: URL?) {
-        self.url = url?.absoluteString ?? ""
-    }
+    let url: URL?
 
     var body: some View {
-        LazyImage(source: url) { state in
+        LazyImage(url: url) { state in
             if let image = state.image {
                 image
             } else {
