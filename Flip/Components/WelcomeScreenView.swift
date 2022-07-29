9fe75a0e9b22602fbd355fbaeb98ea3b136ed784
Jesal Patel <jesalpatelx@gmail.com>
2022-07-29 18:13:35 -0400

    Removed swiping away welcome screen

--- Flip/Components/WelcomeScreenView.swift
+++ Flip/Components/WelcomeScreenView.swift
@@ -14,69 +14,68 @@ struct WelcomeScreenView: View {
 
     var body: some View {
         VStack {
-            VStack {
-                Spacer()
-                Text("Welcome to Flip")
-                    .font(.title.weight(.semibold))
-                    .padding(.bottom, 30)
+            Spacer()
+            Text("Welcome to Flip")
+                .font(.title.weight(.semibold))
+                .padding(.bottom, 30)
 
-                VStack(alignment: .leading, spacing: 20) {
-                    WelcomeCategoryView(
-                        icon: Image(systemName: "text.book.closed.fill"),
-                        iconColor: .indigo,
-                        title: "Library",
-                        content: "Keeps up with books you've read and want to read in the Library tab."
-                    )
+            VStack(alignment: .leading, spacing: 20) {
+                WelcomeCategoryView(
+                    icon: Image(systemName: "text.book.closed.fill"),
+                    iconColor: .indigo,
+                    title: "Library",
+                    content: "Keeps up with books you've read and want to read in the Library tab."
+                )
 
-                    WelcomeCategoryView(
-                        icon: Image(systemName: "magnifyingglass"),
-                        iconColor: .orange,
-                        title: "Search",
-                        // swiftlint:disable:next line_length
-                        content: "Search for new books and add them to the library in the Search tab. Add your own custom books by clicking the plus icon at the top of the page."
-                    )
+                WelcomeCategoryView(
+                    icon: Image(systemName: "magnifyingglass"),
+                    iconColor: .orange,
+                    title: "Search",
+                    // swiftlint:disable:next line_length
+                    content: "Search for new books and add them to the library in the Search tab. Add your own custom books by clicking the plus icon at the top of the page."
+                )
 
-                    WelcomeCategoryView(
-                        icon: Image(systemName: "chart.bar.xaxis"),
-                        iconColor: .blue,
-                        title: "Stats",
-                        content: "Check out some interesting info about your reading habits in the Stats tab."
-                    )
+                WelcomeCategoryView(
+                    icon: Image(systemName: "chart.bar.xaxis"),
+                    iconColor: .blue,
+                    title: "Stats",
+                    content: "Check out some interesting info about your reading habits in the Stats tab."
+                )
 
-                    WelcomeCategoryView(
-                        icon: Image(systemName: "calendar"),
-                        iconColor: .green,
-                        title: "Reading Goal",
-                        // swiftlint:disable:next line_length
-                        content: "Keep up with how many books you want to read this year by setting a reading goal in the Stats tab by clicking the calendar icon at the top of the page."
-                    )
+                WelcomeCategoryView(
+                    icon: Image(systemName: "calendar"),
+                    iconColor: .green,
+                    title: "Reading Goal",
+                    // swiftlint:disable:next line_length
+                    content: "Keep up with how many books you want to read this year by setting a reading goal in the Stats tab by clicking the calendar icon at the top of the page."
+                )
 
-                    WelcomeCategoryView(
-                        icon: Image(systemName: "paintpalette"),
-                        iconColor: .red,
-                        title: "Appearance",
-                        content: "Customize your app by picking a color in the Settings tab (garbage)."
-                    )
-                }
-                Spacer()
-                Button {
-                    // Set to not pop-up again after first time
-                    seenWelcome = true
-                    dismiss()
-                } label: {
-                    HStack {
-                        Text("Continue")
-                            // This method of setting the width is deprecated. Find a better way
-                            .frame(maxWidth: UIScreen.main.bounds.width * 0.8, maxHeight: 26)
-                    }
-                }
-                .tint(.blue)
-                .buttonStyle(.borderedProminent)
-                .cornerRadius(10)
-                .padding(.top, 25)
-                Spacer()
+                WelcomeCategoryView(
+                    icon: Image(systemName: "paintpalette"),
+                    iconColor: .red,
+                    title: "Appearance",
+                    content: "Customize your app by picking a color in the Settings tab (garbage)."
+                )
             }
+            Spacer()
+            Button {
+                // Set to not pop-up again after first time
+                seenWelcome = true
+                dismiss()
+            } label: {
+                HStack {
+                    Text("Continue")
+                        // This method of setting the width is deprecated. Find a better way
+                        .frame(maxWidth: UIScreen.main.bounds.width * 0.8, maxHeight: 26)
+                }
+            }
+            .tint(.blue)
+            .buttonStyle(.borderedProminent)
+            .cornerRadius(10)
+            .padding(.top, 25)
+            Spacer()
         }
+        .interactiveDismissDisabled()
     }
 }
 
